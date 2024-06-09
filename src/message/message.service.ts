520cba2c87d0d5092ab80f42d9431033743b7509
Guilherme Venci <guilhermesanches.av@gmail.com>
2024-06-09 10:32:24 -0300

    Add currentConsupmition var to Client Model / Ajust sendMessage service

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Message, Prisma } from '@prisma/client';
 
@@ -7,6 +7,38 @@ export class MessageService {
   constructor(private prisma: PrismaService) {}
 
   async sendMessage(data: Prisma.MessageCreateInput): Promise<Message> {
+    const client = await this.prisma.client.findUnique({
+      where: { id: data.client.connect.id },
+    });
+
+    if (!client) {
+      throw new Error('Client not found');
+    }
+
+    const messageCost = 0.25;
+
+    if (!data.isWhatsApp) {
+      if (client.plan === 'PREPAID') {
+        if (client.credits < messageCost) {
+          throw new BadRequestException('Insufficient credits');
+        }
+
+        await this.prisma.client.update({
+          where: { id: client.id },
+          data: { credits: client.credits - messageCost },
+        });
+      } else if (client.plan === 'POSTPAID') {
+        if (client.currentConsumption + messageCost > client.limit) {
+          throw new BadRequestException('Consumption limit exceeded');
+        }
+
+        await this.prisma.client.update({
+          where: { id: client.id },
+          data: { currentConsumption: client.currentConsumption + messageCost },
+        });
+      }
+    }
+
     return this.prisma.message.create({
       data: {
         phoneNumber: data.phoneNumber,
